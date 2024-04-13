c6c92919ecd506bdb4da0c372cec114381995a06
AW0723 <64997746+AW0723@users.noreply.github.com>
2024-04-13 14:21:24 -0500

    Added test list view

--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -18,8 +18,8 @@ public class PlayerCamera : MonoBehaviour
     void Start()
     {
         // Centers and hide cursor
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
 
         inputController = GetComponent<InputController>();
     }
