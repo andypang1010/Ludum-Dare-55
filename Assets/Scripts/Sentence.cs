49c3dda9578b627e2cebd1f50bf6976f843d97b8
Andy Pang <andypang1010@gmail.com>
2024-04-14 03:10:37 -0400

    Improve player head bob

--- Assets/Scripts/Sentence.cs
+++ Assets/Scripts/Sentence.cs
@@ -8,24 +8,12 @@ public class Sentence : MonoBehaviour
     private NPCObject npcObject;
     private string sentence;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Setup(NPCObject npcObj, string sentence)
     {
         TMP_Text textMeshPro = GetComponentInChildren<TMP_Text>();
         textMeshPro.text = sentence;
         this.sentence = sentence;
-        this.npcObject = npcObj;
+        npcObject = npcObj;
     }
 
     public void Guess()
